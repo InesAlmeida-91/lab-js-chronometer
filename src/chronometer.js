fe6f5744a00f11bee7f3f4036e0c765d064f3333
Inês Almeida <ines_almeida_203@hotmail.com>
2023-04-26 23:00:56 +0100

    done except bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,48 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    if(printTimeCallback){
+      setInterval(printTimeCallback, 1000)
+    }
+      this.intervalId = setInterval((printTimeCallback) => {
+      this.currentTime++;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    if(this.currentTime === 0) {
+      return 0;
+    } else 
+    return this.currentTime = Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    if(this.currentTime === 0) {
+      return 0;
+    } else {
+      return this.currentTime % 60;
+   }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10) {
+      return "0" + value;
+    } else {
+      return value.toString();
+    }
+    
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId); 
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
